df1198e8e71252ee4b563545a2112e817f3ef16d
macbook <12345>
2022-08-28 20:30:44 +0300

    Start mirage implementation

--- src/module/test/TestPage.tsx
+++ src/module/test/TestPage.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 import Order, {getOrderStr} from "../profile/domain/model/Order";
 
+import { createServer, Factory, Model } from "miragejs"
+
+export function MockServer({enironment = 'development'}){
+    return createServer(
+        {routes: () => {
+
+            }}
+    )
+}
+
+let server = createServer({
+});
+//server.get("/api/users", { users: [{ id: 1, name: "Bob" }] })
+
 const TestPage: React.FC = () => {
 
     const obj = JSON.parse(getOrderStr) as Order;
