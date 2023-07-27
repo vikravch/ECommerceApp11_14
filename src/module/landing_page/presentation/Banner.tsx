03ccbace4dd7db5eb27e2d1b18589531bc5caf79
MayaVi <maya.vi.sand@gmail.com>
2023-07-27 17:23:45 +0300

    Banner css

--- src/module/landing_page/presentation/Banner.tsx
+++ src/module/landing_page/presentation/Banner.tsx
@@ -9,33 +9,35 @@ import nikeImg from "../images/zoomx-invincible-run-flyknit-mens-road-running-sh
 const Banner = () => {
     return (
         <div className={style.rectangle + ' d-sm-none d-md-flex'}>
-            <div className={style.air}>AIR Beyond Compare</div>
-            <div className={style.explore_best_air}>Explore the best Air Max for fall & beyond</div>
-            <div className={style.nike}>NIKE</div>
-            <div className="{style.grad}">
-                <div className={style.grad_vector1}>
-                    <div className={style.grad_vector2}>
-                        <img src={nikeImg} className={style.img_nike}/>
+            <div className={'container'}>
+                <div className={style.air}>AIR Beyond Compare</div>
+                <div className={style.explore_best_air}>Explore the best Air Max for fall & beyond</div>
+                <div className={style.nike}>NIKE</div>
+                <div className="{style.grad}">
+                    <div className={style.grad_vector1}>
+                        <div className={style.grad_vector2}>
+                            <img src={nikeImg} className={style.img_nike}/>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className={style.group2}>
-                <div className={style.text_explore}>Explore</div>
-                <div className={style.arrow_right}>
-                    <img src={arrow_right} className={style.arrow_vector}/>
+                <div className={style.group2}>
+                    <div className={style.text_explore}>Explore</div>
+                    <div className={style.arrow_right}>
+                        <img src={arrow_right} className={style.arrow_vector}/>
+                    </div>
                 </div>
-            </div>
-            <button className={style.btn}>
-                <div className={style.btn_text_states}>
-                    <h3 className={style.btn_text}>Buy now</h3>
+                <button className={style.btn}>
+                    <div className={style.btn_text_states}>
+                        <h3 className={style.btn_text}>Buy now</h3>
+                    </div>
+                </button>
+                <div className={style.icon_left}>
+                    <img src={left} className={style.icon_left_vector}/>
                 </div>
-            </button>
-            <div className={style.icon_left}>
-                <img src={left} className={style.icon_left_vector}/>
-            </div>
-            <div className={style.icon_right}>
-                <img src={right} className={style.icon_right_vector}/>
+                <div className={style.icon_right}>
+                    <img src={right} className={style.icon_right_vector}/>
+                </div>
             </div>
         </div>
 
--- src/module/landing_page/presentation/Blog.tsx
+++ src/module/landing_page/presentation/Blog.tsx
@@ -1,16 +1,11 @@
 import React, {useEffect} from 'react';
 import styles from "../styles/blog.module.scss";
-import blog2 from '../images/blog/blog_img_2.png'
-import blog1 from '../images/blog/blog_img_1.png'
-import clock from '../images/blog/clock.png'
 import ArticleItem from "../../blog_page/presentation/ArticleItem/ArticleItem";
 import formatDate from "../../blog_page/dateTransformer";
 import ArticleInfo from "../../blog_page/domain/model/ArticlesList";
 import {useDispatch, useSelector} from "react-redux";
 import {Store} from "../../../general/redux/storeTypes";
 import {getArticlesListAction} from "../../blog_page/redux/asyncActions";
-import {blogArticles} from "../../blog_page/data/fake_api/articlesList";
-
 
 const Blog = () => {
     const articlesList = useSelector<Store, Array<ArticleInfo>>(state => state.blogPage.articlesList)
@@ -24,7 +19,7 @@ const Blog = () => {
     return (
         <div>
 
-            <div className={styles.container}>
+            <div className={styles.container + ' container'}>
                 <h1 className={styles.h1}>Blog</h1>
                 <div className={styles.blog_items}>
                     {articlesList.slice(0, 3).map((item) => (
