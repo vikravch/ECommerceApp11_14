ed31da68665d88b8c1c5015149476025358a53ee
MayaVi <maya.vi.sand@gmail.com>
2023-07-28 19:25:24 +0300

    Categories new html,Link & Css adaptive

--- src/module/landing_page/presentation/Categories.tsx
+++ src/module/landing_page/presentation/Categories.tsx
@@ -1,86 +1,103 @@
 import React from 'react';
 import style from "../styles/categories.module.css";
 import vector from "../images/group11/vector.png"
+import {Link} from "react-router-dom";
+import styles from "../styles/newArrivals.module.css";
 
 const Categories = () => {
     return (
-        <div className={style.categories_container + ' row'}>
-            <h1>Categorrries</h1>
-            <div className={style.container_for_big + ' col'}>
-                <div className={style.big}>
-                    <div className={style.img1}>
-                        <div className={style.text_men}>Men</div>
-                        <div className={style.group2}>
-                            <div className={style.explore}>Explore</div>
-                            <div className={style.arrow}>
-                                <img src={vector} className={style.arrow_vector}/>
+        <>
+            <h1 className={styles.h1}>Categories</h1>
+            <div className={style.container}>
+
+                <div className={style.bigSubContainer}>
+                    <Link className={style.link} to={'/catalog/men'}>
+                        <div className={style.bigCategoryItem}>
+                            <div className={style.image_Big_Container}>
+                                <img src="image-url.jpg" alt="Image"/>
+                            </div>
+                            <div className={style.textContainer}>
+                                <div className={style.titleBig}>Men</div>
+                                <div className={style.exploreBtn}>
+                                    <div className={style.explore}>Explore</div>
+                                    <div className={style.arrow}>
+                                        <img src={vector} className={style.arrow_vector}/>
+                                    </div>
+                                    <div/>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </Link>
+
+                </div>
+                <div className={style.smallSubContainer}>
+                    <Link className={style.link} to={'/catalog/women'}>
+                        <div className={style.smallCategoryItem}>
+                            <div className={style.image_Small_Container}><img src="#" alt="img"/></div>
+                            <div className={style.textContainer}>
+                                <div className={style.titleSmall}>Women</div>
+                                <div className={style.exploreBtn}>
+                                    <div className={style.explore}>Explore</div>
+                                    <div className={style.arrow}>
+                                        <img src={vector} className={style.arrow_vector}/>
+                                    </div>
+                                    <div/>
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link className={style.link} to={'/catalog/sale'}>
+                        <div className={style.smallCategoryItem}>
+                            <div className={style.image_Small_Container}><img src="#" alt="img"/></div>
+                            <div className={style.textContainer}>
+                                <div className={style.titleSmall}>Sale</div>
+                                <div className={style.exploreBtn}>
+                                    <div className={style.explore}>Explore</div>
+                                    <div className={style.arrow}>
+                                        <img src={vector} className={style.arrow_vector}/>
+                                    </div>
+                                    <div/>
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link className={style.link} to={'/catalog/kids'}>
+                        <div className={style.smallCategoryItem}>
+                            <div className={style.image_Small_Container}><img src="#" alt="img"/></div>
+                            <div className={style.textContainer}>
+                                <div className={style.titleSmall}>Kids</div>
+                                <div className={style.exploreBtn}>
+                                    <div className={style.explore}>Explore</div>
+                                    <div className={style.arrow}>
+                                        <img src={vector} className={style.arrow_vector}/>
+                                    </div>
+                                    <div/>
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                    <Link className={style.link} to={'/catalog/collection'}>
+                        <div className={style.smallCategoryItem}>
+                            <div className={style.image_Small_Container}><img src="#" alt="img"/></div>
+                            <div className={style.textContainer}>
+                                <div className={style.titleSmall}>Collections</div>
+                                <div className={style.exploreBtn}>
+                                    <div className={style.explore}>Explore</div>
+                                    <div className={style.arrow}>
+                                        <img src={vector} className={style.arrow_vector}/>
+                                    </div>
+                                    <div/>
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+
+
                 </div>
             </div>
-
-            <div className={style.container_for_small + ' col'}>
-
-                <div className={'row'}>
-                    <div className={'col-6'}>
-                        <div className={style.small}>
-                            <div className={style.img2}>
-                                <div className={style.text}>Women</div>
-                                <div className={style.group2}>
-                                    <div className={style.explore}>Explore</div>
-                                    <div className={style.arrow}>
-                                        <img src={vector} className={style.arrow_vector}/>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className={style.small}>
-                            <div className={style.img3}>
-                                <div className={style.text}>Kids</div>
-                                <div className={style.group2}>
-                                    <div className={style.explore}>Explore</div>
-                                    <div className={style.arrow}>
-                                        <img src={vector} className={style.arrow_vector}/>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div className={'col-6'}>
-                        <div className={style.small}>
-                            <div className={style.img4}>
-                                <div className={style.text}>Sale</div>
-                                <div className={style.group2}>
-                                    <div className={style.explore}>Explore</div>
-                                    <div className={style.arrow}>
-                                        <img src={vector} className={style.arrow_vector}/>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                        <div className={style.small}>
-                            <div className={style.img5}>
-                                <div className={style.text}>Collection</div>
-                                <div className={style.group2}>
-                                    <div className={style.explore}>Explore</div>
-                                    <div className={style.arrow}>
-                                        <img src={vector} className={style.arrow_vector}/>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
+        </>
 
 
-        </div>
     );
 };
 
