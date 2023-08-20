78ddf29a6fe57e02e9ce94e279ceafac7fedfccc
AnastasiaDah <96196101+AnastasiaDah@users.noreply.github.com>
2023-08-20 17:55:41 +0300

    footer, subscribe

--- src/module/landing_page/presentation/Footer.tsx
+++ src/module/landing_page/presentation/Footer.tsx
@@ -2,43 +2,47 @@ import React from 'react';
 import facebook from "../images/footer/Path.png"
 import instagram from "../images/footer/Combined Shape.png"
 import twitter from "../images/footer/Twit.png"
+import {Link} from "react-router-dom";
+import styles from "../../header/presentation/Header.module.css";
 
 const Footer = () => {
     return (
         <footer className="ry-5 bg-dark">
             <div className='container text-md-start p-5'>
                              <div className='row'>
-                                 <div className='col '>
-                                     <h5 className='text-uppercase fw-bold mb-4 text-light'>LOGO</h5>
+                                 <div className='col'>
+                                     <h5><Link className='text-uppercase fw-bold mb-4 text-light text-decoration-none' to={'/'}>LOGO</Link></h5>
                                  </div>
 
-                                 <div  className='mx-auto  col '>
+                                 <div  className='mx-auto col'>
                                      <h5 className=' fw-bold mb-4 text-light'>Products</h5>
-                                     <p><a href='/product' className='text-secondary'>Men</a></p>
-                                     <p><a href='/product' className='text-secondary'>Women</a></p>
-                                     <p><a href='/product' className='text-secondary'>Kids</a></p>
-                                     <p><a href='/product' className='text-secondary'>Sale</a></p>
-                                     <p><a href='/product' className='text-secondary'>Collection </a></p>
+                                     <p><Link className='text-secondary text-decoration-none' to={'/catalog/men'}>Men</Link></p>
+                                     <p><Link className='text-secondary text-decoration-none' to={'/catalog/women'}>Women</Link></p>
+                                     <p><Link className='text-secondary text-decoration-none' to={'/catalog/kids'}>Kids</Link></p>
+                                     <p><Link className='text-secondary text-decoration-none' to={'/catalog/sale'}>Sale</Link></p>
+                                     <p><Link className='text-secondary text-decoration-none' to={'/catalog/collection'}>Collection</Link></p>
                                  </div>
 
-                                 <div className='mx-auto col '>
+                                 <div className='mx-auto col'>
                                      <h5 className='fw-bold mb-4 text-light'>Get help</h5>
-                                     <p><a href='#!' className='text-secondary'>Order status</a></p>
-                                     <p><a href='#!' className='text-secondary'>Shipping and delovery</a></p>
-                                     <p><a href='#!' className='text-secondary'>Returns</a></p>
-                                     <p><a href='#!' className='text-secondary'>Payment options</a></p>
+                                     <p><a href='#!' className='text-secondary text-decoration-none'>Order status</a></p>
+                                     <p><a href='#!' className='text-secondary text-decoration-none'>Shipping and delovery</a></p>
+                                     <p><a href='#!' className='text-secondary text-decoration-none'>Returns</a></p>
+                                     <p><a href='#!' className='text-secondary text-decoration-none'>Payment options</a></p>
                                  </div>
 
-                                 <div  className='mx-auto mb-md-0 col'>
+                                 <div className='mx-auto mb-md-0 col col-md-6 col-lg col-xl'>
                                      <h5 className='fw-bold mb-4 text-light'>Legals</h5>
-                                     <p className='text-secondary'>Terms of services</p>
-                                     <p className='text-secondary'>Privacy policy</p>
+                                     <p className='text-secondary'>
+                                         <a href={'/agreement/termsOfUse'} className='text-secondary text-decoration-none'>Terms of services</a></p>
+                                     <p className='text-secondary'>
+                                         <a href={'agreement/privacyPolicy'} className='text-secondary text-decoration-none'>Privacy policy</a></p>
                                  </div >
 
-                                 <div  className='mx-auto mb-md-0 col-3 '>
+                                 <div  className='mx-auto mb-md-0 col col-md-6 col-lg col-xl'>
                                      <h5 className='fw-bold mb-4 text-light'>Contact</h5>
                                      <p className='text-secondary'>Email</p>
-                                     <p className="text-light">ecommerce@gmail.com</p>
+                                     <p className='text-light text-decoration-none'>ecommerce@gmail.com</p>
                                      <p className='text-secondary'>Phone</p>
                                      <p className="text-primary">972 756 555-0123</p>
                                      <p className='text-secondary'>Address</p>
@@ -46,11 +50,10 @@ const Footer = () => {
                                  </div>
                              </div>
                          </div>
-            <div className="container border-bottom"></div>
-            <div className='container text-md-start mt-5'>
+            <div className='container border-top text-md-start pt-5'>
                              <div className='row'>
-                                 <div className='col '>
-                                    <h5 className='fw-bold mb-4 text-secondary'>Tel Ran © 2022</h5>
+                                 <div className='col'>
+                                     <p className='text-secondary'>Tel Ran © 2022</p>
                                 </div>
 
                                  <div className='col-auto '>
