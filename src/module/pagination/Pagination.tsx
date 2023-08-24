517cb7dce366e22502b8a66610c58c4133e5b4a0
AnastasiaDah <96196101+AnastasiaDah@users.noreply.github.com>
2023-08-25 01:59:22 +0300

    fix pagination render buttons; cursor; scroll landing page

--- src/module/pagination/Pagination.tsx
+++ src/module/pagination/Pagination.tsx
@@ -19,17 +19,17 @@ const Pagination: React.FC<PaginationProps> = (
     }
 
     //const for 2 of ellipsis:
-    const ellipsis1 = <li key={"ellipsis1"}> <span className="page-link" >...</span></li>
-    const ellipsis2 = <li key={"ellipsis2"}> <span className="page-link" >...</span></li>
+    const ellipsis1 = <li key={"ellipsis1"}> <a className="page-link pe-none">...</a></li>
+    const ellipsis2 = <li key={"ellipsis2"}> <a className="page-link pe-none">...</a></li>
     const firstBtn = <li key={0}
                          onClick={() => handlePageChange(0)}
                          className={`page-item`}>
-        <span className="page-link">1</span>
+        <a className="page-link" href={'#'}>1</a>
     </li>
     const lastBtn = <li key={totalPages - 1}
                         onClick={() => handlePageChange(totalPages - 1)}
                         className={`page-item`}>
-        <span className="page-link">{totalPages}</span>
+        <a className="page-link" href={'#'}>{totalPages}</a>
     </li>
 
     const renderPaginationButtons = () => {
@@ -41,7 +41,7 @@ const Pagination: React.FC<PaginationProps> = (
                     <li key={i}
                         onClick={() => handlePageChange(i)}
                         className={`page-item ${i === currentPage ? 'active' : ''}`}>
-                        <span className="page-link">{i + 1}</span>
+                        <a className="page-link" href={'#'}>{i + 1}</a>
                     </li>
                 );
             }
@@ -54,7 +54,7 @@ const Pagination: React.FC<PaginationProps> = (
                         <li key={i}
                             onClick={() => handlePageChange(i)}
                             className={`page-item ${i === currentPage ? 'active' : ''}`}>
-                            <span className="page-link">{i + 1}</span>
+                            <a className="page-link" href={'#'}>{i + 1}</a>
                         </li>
                     );
                 }
@@ -71,7 +71,7 @@ const Pagination: React.FC<PaginationProps> = (
                     buttons.push(<li key={i}
                                      onClick={() => handlePageChange(i)}
                                      className={`page-item ${i === currentPage ? 'active' : ''}`}>
-                        <span className="page-link" >{i + 1}</span>
+                        <a className="page-link" href={'#'}>{i + 1}</a>
                     </li>)
                 }
                 buttons.push(ellipsis2)
@@ -85,7 +85,7 @@ const Pagination: React.FC<PaginationProps> = (
                     buttons.push(<li key={i}
                                      onClick={() => handlePageChange(i)}
                                      className={`page-item ${i === currentPage ? 'active' : ''}`}>
-                        <span className="page-link">{i + 1}</span>
+                        <a className="page-link" href={'#'}>{i + 1}</a>
                     </li>)
                 }
             }
@@ -98,9 +98,13 @@ const Pagination: React.FC<PaginationProps> = (
         <div>
             <nav>
                 <ul className="pagination justify-content-center my-5">
-                    <li className="page-item "><span className="page-link" onClick={() => handlePrevBtnClick(currentPage)}><span>&laquo;</span></span></li>
+                    {currentPage > 0 ?
+                        <li className="page-item"><a className="page-link" href={'#'} onClick={() => handlePrevBtnClick(currentPage)}><span>&laquo;</span></a></li>
+                        : ''}
                     {renderPaginationButtons()}
-                    <li className="page-item"><span className="page-link" onClick={() => handleNextBtnClick(currentPage)}><span>&raquo;</span></span></li>
+                    {currentPage < totalPages - 1 ?
+                        <li className="page-item"><a className="page-link" href={'#'} onClick={() => handleNextBtnClick(currentPage)}><span>&raquo;</span></a></li>
+                        : ''}
                 </ul>
             </nav>
         </div>
