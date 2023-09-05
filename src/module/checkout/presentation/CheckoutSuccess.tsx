325ebdc114cce1d6f7e432dc8e9534971ef5f288
AnastasiaDah <96196101+AnastasiaDah@users.noreply.github.com>
2023-09-05 09:56:33 +0300

    checkout overflow

--- src/module/checkout/presentation/CheckoutSuccess.tsx
+++ src/module/checkout/presentation/CheckoutSuccess.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import {Link} from "react-router-dom";
+import style from "./Checkout.module.css";
 
 const CheckoutSuccess = () => {
     return (
-        <div className="container my-4" style={{maxWidth: 1070}}>
-            <h2>Your order has been created successfully!</h2>
+        <div className={`mb-5 ${style.minHeight}`}>
+            <h2 className={"mb-5"}>Your order has been created successfully!</h2>
+            <Link to="/profile" className="btn btn-lg btn-dark mb-5">
+                Go to orders
+            </Link>
         </div>
     );
 };
