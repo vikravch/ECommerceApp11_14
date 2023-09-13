8c79310b04e5cfb8a4a57003a163aa5c5d287112
MayaVi <maya.vi.sand@gmail.com>
2023-09-13 20:19:37 +0300

    back MOCK blogPage

---
+++ src/module/blog_page/presentation/fake_data/blogFakeData.ts
@@ -0,0 +1,101 @@
+export const headersFake =
+[
+    {
+        "id": "19",
+        "title": "MOCK - How to Choose the Perfect Sportswear: A Guide for Athletes19",
+        "timestampDateMod": "2020.08.09"
+    },
+    {
+        "id": "22",
+        "title": "Workout From Home Routines: Tips and Best Practices",
+        "timestampDateMod": "2020.08.09"
+    },
+    {
+        "id": "16",
+        "title": "How to Choose the Perfect Sportswear: A Guide for Athletes16",
+        "timestampDateMod": "2020.08.09"
+    },
+    {
+        "id": "8",
+        "title": "How to Choose the Perfect Sportswear: A Guide for Athletes8",
+        "timestampDateMod": "2020.08.09"
+    },
+    {
+        "id": "20",
+        "title": "How to Choose the Perfect Sportswear: A Guide for Athletes20",
+        "timestampDateMod": "2020.08.09"
+    }
+]
+
+export const blogItemsFake = {
+    "content": [
+        {
+            "thumbImgUrl": "https://i.imgur.com/HtfRKH5.png",
+            "timestampDateMod": "2021.08.09",
+            "id": "26",
+            "title": "MOCK Why our Training Club?"
+        },
+        {
+            "thumbImgUrl": "https://i.imgur.com/fHi58j5.png",
+            "timestampDateMod": "2022.08.09",
+            "id": "25",
+            "title": "17 Tips for Becoming Sportsmen"
+        },
+        {
+            "thumbImgUrl": "https://i.imgur.com/erhOpDY.png",
+            "timestampDateMod": "2023.08.09",
+            "id": "24",
+            "title": "For Wherever and Whenever You Want To Train"
+        },
+        {
+            "thumbImgUrl": "https://i.imgur.com/7vkobKf.jpg",
+            "timestampDateMod": "2020.08.09",
+            "id": "23",
+            "title": "Reach Your Fitness Goals with the our Training Club App (NTC)."
+        },
+        {
+            "thumbImgUrl": "https://i.imgur.com/nJypwCM.png",
+            "timestampDateMod": "2021.08.09",
+            "id": "22",
+            "title": "Workout From Home Routines: Tips and Best Practices"
+        },
+        {
+            "thumbImgUrl": "https://i.imgur.com/EosIyw1.png",
+            "timestampDateMod": "2022.08.09",
+            "id": "21",
+            "title": "Train The Way You Want: Energise Your Routine with our Growing Library of Workout"
+        }
+    ],
+    "pageable": {
+        "sort": {
+            "empty": false,
+            "sorted": true,
+            "unsorted": false
+        },
+        "offset": 0,
+        "pageSize": 6,
+        "pageNumber": 0,
+        "paged": true,
+        "unpaged": false
+    },
+    "last": false,
+    "totalPages": 5,
+    "totalElements": 26,
+    "size": 6,
+    "number": 0,
+    "sort": {
+        "empty": false,
+        "sorted": true,
+        "unsorted": false
+    },
+    "first": true,
+    "numberOfElements": 6,
+    "empty": false
+}
+
+export const blogItemFake = {
+    "id": "23",
+    "body": "<div><p>If you're looking for a way to take your fitness journey to the next level, our Training Club App (NTC) is here to help. With its innovative features and expert-guided workouts, you'll have everything you need to reach your fitness goals in one convenient app.</p><h3>1. Personalized Workouts Tailored to You</h3><p>Whether you're a beginner or an experienced athlete, the NTC app offers personalized workouts that cater to your fitness level and goals. From strength training to cardio, yoga to HIIT, you can choose from a wide variety of workouts designed to challenge and motivate you.</p><h3>2. Expert Guidance from World-Class Trainers</h3><p>With the NTC app, you'll have access to world-class trainers who will guide you every step of the way. From providing detailed instructions on proper form to offering motivational tips, these trainers will ensure that you get the most out of every workout session.</p><p>With the NTC app, you can track your progress, set goals, and monitor your performance to stay motivated and on track. The app also provides valuable insights and recommendations to help you optimize your training and maximize your results.</p><p>Whether you're looking to lose weight, build strength, increase flexibility, or improve overall fitness, the NTC app has got you covered. So, download the app, put on your favorite sportswear, and get ready to crush your fitness goals with our Training Club App (NTC).</p></div>",
+    "title": "MOCK - Reach Your Fitness Goals with the our Training Club App (NTC).",
+    "timestampDateMod": "2023-07-14T22:59:50.000+00:00"
+}
